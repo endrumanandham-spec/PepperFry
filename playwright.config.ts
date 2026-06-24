b8c9241b8cc080e0049a03b5fdbc21727d3d3899
endrumanandham-spec <endrumanandham@.com>
2026-06-24 18:55:08 +0530

    Login test case

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
+  timeout: 90 * 1000,  
   use: {
     headless: true,
     screenshot: 'on',
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -5,7 +5,21 @@ const envConfig = require(`../config/${process.env.TEST_ENV || "dev"}.env.ts`).d
 test('login works in selected environment', async ({ page }) => {
   const loginPage = new LoginPage(page);
   await page.goto(envConfig.baseURL);
-  await loginPage.gotoLogin();
-  await loginPage.login(envConfig.username, envConfig.password);
-  await expect(loginPage.welcomeMessage).toHaveText(/Welcome/);
+  //await loginPage.gotoLogin();
+  //await loginPage.login(envConfig.username, envConfig.password);
+  await page.waitForLoadState('networkidle');
+  await page.waitForTimeout(10000);
+  const loginPopup= await page.locator('#modal-body');
+  //await loginPopup.waitFor({ state: 'visible' });
+  await expect(page.getByRole('button', { name: 'CONTINUE' })).toBeVisible();
+  await page.waitForTimeout(10000);
+
+  
+  //await loginPopup.locator('[data-test="pepperfryLogo"]').click();
+  /*await expect(page.locator('pf-header-icons')).toContainText('Hi, *');
+  await page.getByRole('link', { name: 'My Profile' }).click();
+  await expect(page.locator('pf-my-details')).toContainText('My Details');
+  await expect(page.locator('pf-default-address')).toContainText('Default Address');
+  await expect(page.locator('pf-my-account-menus')).toContainText('My Orders');
+  */
 });
